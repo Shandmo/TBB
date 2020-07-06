60591ad132d90f50dc79e96a1ff6072bdbc8998e
Barnes Shay <fillerbarnesmail@gmail.com>
2020-07-06 22:36:48 +0000

    Update to ex_twelve.py

--- ex_twelve.py
+++ ex_twelve.py
@@ -2,11 +2,12 @@
 """ Exercise Twelve Computing Simple Interest """
 from decimal import *
 def main():
+    """ Computing Interest through Decimal """
     principal = Decimal(input("Enter the principal: "))
     roi = Decimal(input("Enter the rate of interest: "))
     time = Decimal(input("Enter the number of years: "))
     result = Decimal((principal * (1 + ((roi / 100) * time))))
-    result = Decimal(result).quantize(Decimal('1.00'), rounding = ROUND_UP)
+    result = Decimal(result).quantize(Decimal('1.00'), rounding=ROUND_UP)
     print(f"After {time} years at {roi}%, the investment will be worth ${result}.")
 
 if __name__ == "__main__":
